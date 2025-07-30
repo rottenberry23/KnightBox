00b6281615b86d77a943f21b5e9889e677b7439a
rottenberry23 <raohq8@gmail.com>
2025-07-31 03:00:33 +0500

    Uploaded Files - Knight Sprites + Sprite Object

---
+++ knight-BAK.js
@@ -0,0 +1,113 @@
+const animation = {
+    atk1: {
+        src:"knight/sheets/atk1.png",
+        frameCount: 4,
+        frameDelay: 0.4,
+    },
+    atk2: {
+        src:"knight/sheets/atk2.png",
+        frameCount: 6,
+        frameDelay: 0.7,
+    },
+    atkCombo: {
+        src:"knight/sheets/atkcombo.png",
+        frameCount:10,
+        frameDelay:1.2,
+    },
+    crouchAtk: {
+        src:"knight/sheets/crouch_atk.png",
+        frameCount: 4,
+        frameDelay: 0.6,
+    },
+    crouchWalk: {
+        src:"knight/sheets/crouch_walk.png",
+        frameCount: 8,
+        frameDelay: 1.2,
+    },
+    crouchedCycle: {
+        src:"knight/sheets/crouched_cycle.png",
+        frameCount: 3,
+        frameDelay: 0.2,
+    },
+    crouchedTransition: {
+        src:"knight/sheets/crouched_transition.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    crouched: {
+        src:"knight/sheets/crouched.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    dash: {
+        src:"knight/sheets/dash.png",
+        frameCount: 2,
+        frameDelay: 0.3,
+    },
+    death: {
+        src:"knight/sheets/death.png",
+        frameCount: 10,
+        frameDelay: 1.1,
+    },
+    fall: {
+        src:"knight/sheets/fall.png",
+        frameCount: 3,
+        frameDelay: 0.5,
+    },
+    idle: {
+        src:"knight/sheets/idle.png",
+        frameCount: 10,
+        frameDelay: 1.4,
+    },
+    jumpFall: {
+        src:"knight/sheets/jump_fall.png",
+        frameCount: 2,
+        frameDelay: 0.8,
+    },
+    jump: {
+        src:"knight/sheets/jump.png",
+        frameCount: 3,
+        frameDelay: 0.5,
+    },
+    roll: {
+        src:"knight/sheets/roll.png",
+        frameCount: 12,
+        frameDelay: 0.67,
+    },
+    run: {
+        src:"knight/sheets/run.png",
+        frameCount: 10,
+        frameDelay: 0.79,
+    },
+    slideBetween: {
+        src:"knight/sheets/slide_between.png",
+        frameCount: 2,
+        frameDelay: 0.3,
+    },
+    slideEnd: {
+        src:"knight/sheets/slide_end.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    slideFull: {
+        src:"knight/sheets/slide_full.png",
+        frameCount: 4,
+        frameDelay: 0.4,
+    },
+    slideStart: {
+        src:"knight/sheets/slide_start.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    turnAround: {
+        src:"knight/sheets/turn_around.png",
+        frameCount: 3,
+        frameDelay: 0.58,
+    },
+    wallSlide: {
+        src:"knight/sheets/wall_slide.png",
+        frameCount: 3,
+        frameDelay: 0.4,
+    },
+
+};
---
+++ knight.js
@@ -0,0 +1,113 @@
+const animation = {
+    atk1: {
+        src:"./sprites/atk1.png",
+        frameCount: 4,
+        frameDelay: 0.4,
+    },
+    atk2: {
+        src:"./sprites/atk2.png",
+        frameCount: 6,
+        frameDelay: 0.7,
+    },
+    atkCombo: {
+        src:"./sprites/atkcombo.png",
+        frameCount:10,
+        frameDelay:1.2,
+    },
+    crouchAtk: {
+        src:"./sprites/crouch_atk.png",
+        frameCount: 4,
+        frameDelay: 0.6,
+    },
+    crouchWalk: {
+        src:"./sprites/crouch_walk.png",
+        frameCount: 8,
+        frameDelay: 1.2,
+    },
+    crouchedCycle: {
+        src:"./sprites/crouched_cycle.png",
+        frameCount: 3,
+        frameDelay: 0.2,
+    },
+    crouchedTransition: {
+        src:"./sprites/crouched_transition.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    crouched: {
+        src:"./sprites/crouched.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    dash: {
+        src:"./sprites/dash.png",
+        frameCount: 2,
+        frameDelay: 0.3,
+    },
+    death: {
+        src:"./sprites/death.png",
+        frameCount: 10,
+        frameDelay: 1.1,
+    },
+    fall: {
+        src:"./sprites/fall.png",
+        frameCount: 3,
+        frameDelay: 0.5,
+    },
+    idle: {
+        src:"./sprites/idle.png",
+        frameCount: 10,
+        frameDelay: 1.4,
+    },
+    jumpFall: {
+        src:"./sprites/jump_fall.png",
+        frameCount: 2,
+        frameDelay: 0.8,
+    },
+    jump: {
+        src:"./sprites/jump.png",
+        frameCount: 3,
+        frameDelay: 0.5,
+    },
+    roll: {
+        src:"./sprites/roll.png",
+        frameCount: 12,
+        frameDelay: 0.67,
+    },
+    run: {
+        src:"./sprites/run.png",
+        frameCount: 10,
+        frameDelay: 0.79,
+    },
+    slideBetween: {
+        src:"./sprites/slide_between.png",
+        frameCount: 2,
+        frameDelay: 0.3,
+    },
+    slideEnd: {
+        src:"./sprites/slide_end.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    slideFull: {
+        src:"./sprites/slide_full.png",
+        frameCount: 4,
+        frameDelay: 0.4,
+    },
+    slideStart: {
+        src:"./sprites/slide_start.png",
+        frameCount: 1,
+        frameDelay: 0.1,
+    },
+    turnAround: {
+        src:"./sprites/turn_around.png",
+        frameCount: 3,
+        frameDelay: 0.58,
+    },
+    wallSlide: {
+        src:"./sprites/wall_slide.png",
+        frameCount: 3,
+        frameDelay: 0.4,
+    },
+
+};
